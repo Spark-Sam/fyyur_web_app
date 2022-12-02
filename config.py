36fbc7c119dc26939235c9314c6b3aa622dbad68
Spark Sam <sam.kichwa@gmail.com>
2022-12-03 00:47:12 +0300

    config.py

--- config.py
+++ config.py
@@ -7,10 +7,11 @@ DEBUG = True
 
 # Connect to the database
 load_dotenv()
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
+#DB_NAME = os.environ.get("DB_NAME")
+#DB_USER = os.environ.get("DB_USER")
+#DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@localhost:5432/{}'.format(DB_USER, DB_PASSWORD, DB_NAME)
+#SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@localhost:5432/{}'.format(DB_USER, DB_PASSWORD, DB_NAME)
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
