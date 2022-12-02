6cad75c5819d570b037fb12acbf19cc4ef227d12
Spark Sam <sam.kichwa@gmail.com>
2022-12-02 23:58:42 +0300

    enhanced credential handling in config.py to use a .env file.

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
-SECRET_KEY = os.urandom(32)
-# Grabs the folder where the script runs.
-basedir = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
+
 
 # Enable debug mode.
 DEBUG = True
 
 # Connect to the database
-database_user = DB_USER #Enter the user_name you used to create the db (fyyur)
-database_pass = DB_PASSWORD #Enter the password you created
-database_url = 'localhost:5432'
-database_name = DB_NAME #Enter the db_name. It should be "fyyur"
+load_dotenv()
+#DB_NAME = os.environ.get("DB_NAME")
+#DB_USER = os.environ.get("DB_USER")
+#DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}/{}'.format(database_user, database_pass, database_url, database_name)
+#SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@localhost:5432/{}'.format(DB_USER, DB_PASSWORD, DB_NAME)
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
